1078b57685942d2f931cef0627379ae76d01ead6
Rob Romito <rob@robertromito.com>
2012-01-31 21:15:58 -0500

    More theme fiddling

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
- '(custom-enabled-themes (quote (manoj-dark)))
+ '(custom-enabled-themes nil)
  '(display-time-mode t)
  '(electric-indent-mode t)
  '(electric-layout-mode t)
